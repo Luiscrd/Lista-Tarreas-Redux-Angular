cb1f34e5d59e904c19d20da6617c373975259286
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-23 12:49:57 +0100

    upload

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodoAddComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent
+  ],
   imports: [
     CommonModule
   ]
